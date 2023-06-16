5f795f534cd98e5df7fa0303f088582b72496d85
Mockelmann <ryguy.in.ne@gmail.com>
2023-06-16 03:14:43 -0500

    Hours for Dash Hound

--- JS/gundamDataSet.js
+++ JS/gundamDataSet.js
@@ -120,7 +120,7 @@ const projects = [
     ['Calamitous Godzilla',[1,'Calamity'],'FM',false,false,false,'Seed','B'],
     ['PB Armageddon',[2.5,'Dagger L','Dagger L','Dagger Expansion Pack'],'HG',false,true,false,'Seed','E',"2022-03",28.5,false,'Builds/Armageddon.html',[''],"2022-05-23"],
     ['King Leir',[1,'Dagger L'],'HG',false,false,false,'Seed','E',"2021-06",14.09,false,'Builds/KingLeir.html',[''],"2021-06-26"],
-    ['Samus: Varia Suit',[1,'Dagger L'],'HG',false,false,false,'Seed','C',"2023-06-13",2.5,false,'Builds/VariaSuit.html',['Tamiya: Gold TS-21 Can']],
+    ['Samus: Varia Suit',[1,'Dagger L'],'HG',false,false,false,'Seed','C',"2023-06-13",3.0,false,'Builds/VariaSuit.html',['Tamiya: Gold TS-21 Can']],
     ['Charizard',[1,'Destiny'],'MG',false,false,false,'Seed','B'],
     ['Charizard X',[1,'Destiny'],'SD',false,false,false,'Seed','E',"2021-10",8.13,false,'Builds/CharizardX.html',[''],"2022-05-16"],
     ['Iapetus',[1,'Destiny'],'SD',false,false,false,'Seed','B'],
@@ -322,7 +322,7 @@ const projects = [
     ['Wilbee',[1,'Bumblebee'],'Transformer',false,false,false,'','E',"2022-11",9.33,false,'Builds/Wilbee.html',[''],"2023-01-09"],
     
     //Zoid
-    ['Dash Hound',[1,'Shadow Fox'],'Zoid',false,false,false,'','C',"2023-06-12",1.5,false,'Builds/DashHound.html',['']],
+    ['Dash Hound',[1,'Shadow Fox'],'Zoid',false,false,false,'','C',"2023-06-12",6.5,false,'Builds/DashHound.html',['']],
     ['Shere Khan',[1,'Blade Liger'],'Zoid',false,false,false,'','B'],
     ['Akela',[1,'Konig Wolf Heavy Arms'],'Zoid',false,false,false,'','A'],
     ['Kipling',[1,'Liger Zero X'],'Zoid',false,false,false,'','B']
