849b407bd645c25a438f3090ef8ff6843d7db1af
Mockelmann <ryguy.in.ne@gmail.com>
2023-07-04 21:16:23 -0500

    Pushing latest batch

---
+++ templates/model_gundam.js
@@ -0,0 +1,36 @@
+[
+    ID, //numeric
+    1,
+    'Short Name',
+    'Full Name',
+    'Serial Number',
+    'Series',
+    EW,    //boolean
+    ['Developed From'],
+    ['Developed Into'],
+    'Era',
+    [StartDate,'Start Date in string',EndDate],
+    'Pilot',
+    ['Second Pilots'],
+    'Manufacturer',
+    ['Affiliations'],
+    [16.7,'16.7 Meters'],           //Height
+    [7.7,'7.7 Metric Tons'],        //Weight
+    'Material / Armor',
+    'Energy / Power Source',
+    [
+        'Armaments'
+    ],
+    [
+        'Equipment'
+    ],
+    [
+        'Systems'
+    ],
+    'Designed by',
+    SDWHero,                //boolean
+    'Alternate/nickname',
+    MobileArmor,            //boolean
+    ['Model photo Url','Photo Alt Text'],
+    Is_Gundam           //boolean
+]
